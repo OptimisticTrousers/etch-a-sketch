9e226a412d23e63a0e6d0e444e05d6b72206d38e
OptimisticTrousers <tony.isern@yahoo.com>
2022-03-12 16:11:54 -0500

    Added a nested for loop to render a grid of elements. In this case, 16x16 and added eventlisteners for a mouseover effect for each of the divs. Each div now changes from yellow to white on a mouseover event

--- script.js
+++ script.js
@@ -3,20 +3,56 @@ let attempts = 0;
 const container = document.querySelector('.container');
 
 
-container.style["background-color"] = "yellow";
-
-container.appendChild('div');
 
 function createPad(){
 
     for(let i = 0; i < 16; i++){
 
+        const div = document.createElement('div');
 
-        let div = document.createElement('div');
+        div.style.height ="100px";
+
+        div.style.width = "100px";
+
+        div.style.backgroundColor = "yellow";
+
+        div.style.border = "1px solid black"
+
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = "white";
+        })
+        
+
+        if(i != 0){
+
+            div.style.cssFloat = "clear"
+            container.appendChild(div);
+        }
+
+    for(let j = 0; j < 16; j++){
+
+        const div = document.createElement('div');
+
+        div.style.height ="100px";
+
+        div.style.width = "100px";
+
+        div.style.backgroundColor = "yellow";
+
+        div.style.border = "1px solid black"
+
+        div.style.cssFloat = "left"
+
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = "white";
+        })
 
         container.appendChild(div);
 
+        }
+
     }
+
 }
 
 createPad();
