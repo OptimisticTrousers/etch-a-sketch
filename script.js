dc23920a9c8cb89cc3c29f7589304dcaf8935786
OptimisticTrousers <tony.isern@yahoo.com>
2022-07-16 12:46:02 -0400

    Adding favicon

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ const slider = document.getElementById('slider');
 const userColorPicker = document.getElementById('picker')
 const backgroundColorPicker = document.getElementById('background-picker')
 
-let screenWidth = 400;
-let screenHeight = 400;
+let screenWidth = 100;
+let screenHeight = 100;
 
 const mediaQuery = window.matchMedia('(max-width: 890px)');
 
